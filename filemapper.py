e269a9d5aa2569bd5504a49097c430beda3da264
Ethan <eboileau@uiowa.edu>
2025-09-30 14:30:47 -0500

    made a lil file tree shower thing using the os modile

---
+++ filemapper.py
@@ -0,0 +1,52 @@
+import os
+from colorama import init
+from termcolor import colored
+
+init()
+
+file_tree = ""
+
+path = "C:/Users/ezenb/Projects/python/filetree/testfolder"
+parent_directory = "<" + os.path.basename(path) +">"
+
+def scan_directory_recursive(path, depth=0):
+    global file_tree
+    for x in os.scandir(path):
+        
+        if x.is_dir():
+            file_tree += colored( " " * depth +  f"<{x.name}>\n", text_color(depth))
+            scan_directory_recursive(x.path, depth + 2)
+        else:
+                file_tree += colored(" " * depth +  f"--{x.name}\n", text_color(depth))
+                #print(x.path)
+    
+
+
+def text_color(depth):
+    if depth == 0:
+         return 'green'
+    elif depth == 2:
+         return 'blue'
+    elif depth % 2 == 0:
+         return 'red'
+    elif depth % 3 == 0:
+         return 'yellow'
+    else:
+        return 'red'
+
+
+scan_directory_recursive(path)
+print(colored(parent_directory, "yellow" )+"\n"+ file_tree)
+
+def scan_directory(path):
+    global file_tree
+    file_tree += f" \n | {parent_directory}"
+    for x in os.scandir(path):
+        if x.is_dir():
+              file_tree += f" \n | {x}"
+              for y in os.scandir(x):
+                   file_tree += f"--{y}"
+        else:
+            file_tree += f" \n | {x}"
+    print(file_tree)
+
